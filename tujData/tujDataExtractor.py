fdc8f131e24dcfc080c86c59ad3014cbb40bdbb0
Laird Streak <Laird.Streak@aranzgeo.com>
2017-10-19 15:44:33 +1300

    Allow different categories

--- tujData/tujDataExtractor.py
+++ tujData/tujDataExtractor.py
@@ -2,17 +2,16 @@
 # -*- coding: utf8 -*-
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import pygal
 
 __author__ = "Laird Streak"
 
 def fetch_tuj_data():
-    fetch_tuj_alchemy()
+    fetch_tuj_category("alchemy")
+    fetch_tuj_category("inscription")
 
 
-def fetch_tuj_alchemy():
-    url = ("https://theunderminejournal.com/#us/khazgoroth/category"
-          "/alchemy")
+def fetch_tuj_category(category):
+    url = "https://theunderminejournal.com/#us/khazgoroth/category/" + category
     driver = webdriver.PhantomJS(executable_path="./driver/phantomjs")
     driver.get(url)
     content_data = driver.page_source
@@ -25,7 +24,7 @@ def fetch_tuj_alchemy():
             tdcells = row.find_all("td")
             lengthArray = len(tdcells)
             if lengthArray > 0:
-                print("td")
+                print(tdcells[1].text + " " + tdcells[2].text + " " + tdcells[3].text + " " + tdcells[4].text + " " + tdcells[5].text)
             else:
                 thcells = row.find_all("th")
                 lenheaders = len(thcells)
@@ -34,8 +33,8 @@ def fetch_tuj_alchemy():
                    if theader is not None:
                       print(theader.contents[0])
                 else:
-                    print(thcells[0].text + " " + thcells[1].text)            
-        #print(divitem)
+                    print(thcells[0].text + " " + thcells[1].text + " " + thcells[2].text + " " + thcells[3].text + " " + thcells[4].text)            
+
 
 if __name__ == '__main__':
     fetch_tuj_data()
