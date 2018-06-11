45429e6b24183789f574689656cad301be25198b
lairdstreak <lairdstreak@gmail.com>
2018-06-11 22:58:19 +1200

    stashpop

--- pySelenium/flrun.py
+++ pySelenium/flrun.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from flask import Flask
 from flask import render_template
 from flask_debug import Debug
@@ -24,4 +25,32 @@ def raw():
     return app.send_static_file("hello.htm")
 
 Debug(app)
+=======
+from flask import Flask
+from flask import render_template
+from flask_debug import Debug
+
+app = Flask(__name__)
+
+print(__name__)
+
+@app.route('/')
+def default_route():
+    return render_template('default.html')
+
+@app.route('/index')
+def index():
+    return '<div>Here</div>'
+
+@app.route('/template')
+@app.route('/template/<name>')
+def template(name=None):
+    return render_template('template.html', name=name)
+
+@app.route('/raw')
+def raw():
+    return app.send_static_file("hello.htm")
+
+Debug(app)
+>>>>>>> Stashed changes
 app.run(port=1000)
