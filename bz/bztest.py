cd8fb7916f22edb08c93ef53c0ce82dd504d49b8
Laird Streak <LairdStreak@gmail.com>
2018-09-03 20:29:32 +1200

    update 1

--- bz/bztest.py
+++ bz/bztest.py
@@ -6,17 +6,20 @@ from pyblizzard.wow.enum.characterprofilefield import CharacterProfileField
 from pyblizzard.wow.enum.pvpbracket import PvpBracket
 
 def main():
-    pybz = PyBlizzard("zxcr", Region.US, Locale.US)
+    pybz = PyBlizzard("gfyjawteeb4wbzke674wbwzyjbpcahjv", Region.US, Locale.US)
     # CHARACTER PROFILE
-    print('Getting character profile...')
-    character_profile = pybz.wow.get_character_profile("khazgoroth", "Gnomeifix", CharacterProfileField.TALENTS, CharacterProfileField.ITEMS)
-    util.print_response_object(character_profile)
+    # print('Getting character profile...')
+    # character_profile = pybz.wow.get_character_profile("khazgoroth", "Gnomeifix", CharacterProfileField.TALENTS, CharacterProfileField.ITEMS)
+    # util.print_response_object(character_profile)
 
 
      # GUILD PROFILE
     print('Getting guild profile...')
     guild_profile = pybz.wow.get_guild_profile("dathremar", "WhiteHand")
-    util.print_response_object(guild_profile)
+    for char in guild_profile['members']:
+        print(f"{char['character']['name']} - {char['character']['level']}")
+        # print(char['character']['level'])
+    #util.print_response_object(guild_profile)
 
 
 
