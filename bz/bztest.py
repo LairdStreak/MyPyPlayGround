64b874659e4860bf7499fe2d78e02fee9171ba52
Laird Streak <laird.streak@seequent.com>
2018-09-03 15:54:11 +1200

    bztest

---
+++ bz/bztest.py
@@ -0,0 +1,24 @@
+from pyblizzard.common.enum.locale import Locale
+from pyblizzard.common.enum.region import Region
+from pyblizzard.common.utility import util
+from pyblizzard.pyblizzard import PyBlizzard
+from pyblizzard.wow.enum.characterprofilefield import CharacterProfileField
+from pyblizzard.wow.enum.pvpbracket import PvpBracket
+
+def main():
+    pybz = PyBlizzard("zxcr", Region.US, Locale.US)
+    # CHARACTER PROFILE
+    print('Getting character profile...')
+    character_profile = pybz.wow.get_character_profile("khazgoroth", "Gnomeifix", CharacterProfileField.TALENTS, CharacterProfileField.ITEMS)
+    util.print_response_object(character_profile)
+
+
+     # GUILD PROFILE
+    print('Getting guild profile...')
+    guild_profile = pybz.wow.get_guild_profile("dathremar", "WhiteHand")
+    util.print_response_object(guild_profile)
+
+
+
+if __name__ == '__main__':
+    main()
--- tujProfessionTopItems/qttest.py
+++ tujProfessionTopItems/qttest.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
  
 class App(QWidget):
  
@@ -8,7 +9,7 @@ class App(QWidget):
         super().__init__()
         self.title = 'PyQt5 simple window - pythonspot.com'
         self.left = 10
-        self.top = 10
+        self.top = 50
         self.width = 640
         self.height = 480
         self.initUI()
@@ -16,7 +17,16 @@ class App(QWidget):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        button = QPushButton('PyQt5 button', self)
+        button.setToolTip('This is an example button')
+        button.move(100,70) 
+        button.clicked.connect(self.on_click)
         self.show()
+ 
+    @pyqtSlot()
+    def on_click(self):
+        print('PyQt5 button click')
+
  
 if __name__ == '__main__':
     app = QApplication(sys.argv)
