0e6fa3064958c0a456c8cbdfb5656bd6c4211f54
Laird Streak <laird.streak@seequent.com>
2018-09-04 14:40:09 +1200

    excelwriter FTW ! :rocket:

--- bz/bztest.py
+++ bz/bztest.py
@@ -4,6 +4,8 @@ from pyblizzard.common.utility import util
 from pyblizzard.pyblizzard import PyBlizzard
 from pyblizzard.wow.enum.characterprofilefield import CharacterProfileField
 from pyblizzard.wow.enum.pvpbracket import PvpBracket
+import pandas as pd
+import xlsxwriter
 
 def main():
     pybz = PyBlizzard("gfyjawteeb4wbzke674wbwzyjbpcahjv", Region.US, Locale.US)
@@ -16,8 +18,22 @@ def main():
      # GUILD PROFILE
     print('Getting guild profile...')
     guild_profile = pybz.wow.get_guild_profile("dathremar", "WhiteHand")
-    for char in guild_profile['members']:
-        print(f"{char['character']['name']} - {char['character']['level']}")
+    data = pd.DataFrame(guild_profile['members'])
+    list = []
+    for index, row in data.iterrows():
+        list.append({'Name' : row['character']['name'], 'Level' : row['character']['level']})
+
+    df = pd.DataFrame.from_records(list)    
+    writer = pd.ExcelWriter('simple-report.xlsx', engine='xlsxwriter')
+    df.to_excel(writer, index=False)
+    writer.save()
+        
+    #for item in list:
+    #    print(f"{item['Name']} - {item['Level']}")
+
+
+    #for char in guild_profile['members']:
+    #    print(f"{char['character']['name']} - {char['character']['level']}")
         # print(char['character']['level'])
     #util.print_response_object(guild_profile)
 
