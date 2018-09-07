49fcf0ba8cd2625d44d3327c86aefb92451660c7
Laird Streak <laird.streak@seequent.com>
2018-09-07 14:01:59 +1200

    Add Class and level and Race Filters

--- bz/bztest.py
+++ bz/bztest.py
@@ -8,30 +8,35 @@ import pandas as pd
 import xlsxwriter
 import matplotlib.pyplot as plt
 
+
+
+def GetWoWRaces(pybz):
+    print('Getting character races...')
+    pyb_races = pybz.wow.get_data_character_races()
+    return pyb_races
+
+def GetWoWClasses(pybz):
+    print('Getting character classes...')
+    pyb_classes = pybz.wow.get_data_character_classes()
+    return pyb_classes    
+
 def main():
     pybz = PyBlizzard("gfyjawteeb4wbzke674wbwzyjbpcahjv", Region.US, Locale.US)
-    
+    toon_races = pd.DataFrame(GetWoWRaces(pybz))
+    toon_classes = pd.DataFrame(GetWoWClasses(pybz))
      # GUILD PROFILE
     print('Getting guild profile...')
     guild_profile = pybz.wow.get_guild_profile("dathremar", "WhiteHand")
     data = pd.DataFrame(guild_profile['members'])
     list = []
     for index, row in data.iterrows():
-        list.append({'Name' : row['character']['name'], 'Level' : row['character']['level']})
+        raceint = row['character']['race']
+        classint = row['character']['class']
+        race = [i for i in toon_races.races if i['id'] == raceint][0]['name']
+        toonclass = [i for i in toon_classes.classes if i['id'] == classint][0]['name']
+        list.append({'Name' : row['character']['name'], 'Level' : row['character']['level'], 'Race' : race, 'Class' : toonclass})
 
-    df = pd.DataFrame.from_records(list)   
-    df = df[df['Level'] > 115]
-
-    df.set_index('Name')
-    
-    labels = df.index.values
-    values = df['Level']
-    fig1, ax1 = plt.subplots()
-    ax1.pie(values, labels = labels)
-    ax1.axis('equal')
-    plt.show()
-
-
+    df = pd.DataFrame.from_records(list)  
     writer = pd.ExcelWriter('simple-report.xlsx', engine='xlsxwriter')
     df.to_excel(writer, index=False)
     writer.save()
